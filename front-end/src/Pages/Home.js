a011967a97e61f621b7f2d8bc3a5500f53a54199
Ritvik Thakur <ritvik.thakur04@gmail.com>
2025-04-16 11:49:02 -0400

    Fixed small issue with signup page and made temp home page

---
+++ front-end/src/Pages/Home.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from '../Components/Navigation';
+
+
+const Home = () => {
+  return (
+    <div className="App">
+        <Navigation/>
+
+      <h1>
+        Hello this is the home page.
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
