cd83c18e03a9bdcb19d5f3d9bb17d579d609969c
laurenchipdey <laurenchipdey@gmail.com>
2025-04-16 21:18:05 -0400

    Update index.js
    set up server and listen to input from front end server

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ const upload = multer()            // store file in memory
 AWS.config.update({
   accessKeyId:     process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region:          process.env.AWS_REGION,
+  region:         process.env.AWS_REGION,
 })
 const s3 = new AWS.S3()
 
