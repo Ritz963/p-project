303bc7ebd0d275267955241614123f1c716ab903
Ritvik Thakur <ritvik.thakur04@gmail.com>
2025-04-20 02:00:44 -0400

    Made modal

--- front-end/src/Components/ActionButtons.js
+++ front-end/src/Components/ActionButtons.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const ActionButtons = () => {
+const ActionButtons = ({ onUploadClick }) => {
   return (
     <div className="action-buttons">
       <button className="action-btn save-btn">Save Outfit</button>
       <button className="action-btn randomize-btn">Randomize</button>
-      <button className="action-btn upload-btn">Upload</button>
+      <button className="action-btn upload-btn" onClick={onUploadClick}>
+        Upload
+      </button>
     </div>
   );
 };
